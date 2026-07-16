92f46ab06e013461db8edbae7a8e74242f54fcbc
Archana <167700685+Dev-Archana@users.noreply.github.com>
2026-07-16 16:57:06 +0530

    Implement even number check using bitwise operator

---
+++ EvenOrOdd.c
@@ -0,0 +1,15 @@
+//Program to check the number is even or not
+#include <stdio.h>
+
+int main()
+{
+    // int a=10;
+    // if(a%2==0){
+    //     printf("even");
+    // }
+    int a=10;
+    if((a&1)==0){
+        printf("even");
+    }
+    return 0;
+}
