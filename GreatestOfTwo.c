42852bc8e10615d375c31514c51c72f38207d11e
Archana <167700685+Dev-Archana@users.noreply.github.com>
2026-07-16 16:58:17 +0530

    Add program to find greatest of two numbers
    Implemented a program to find the greatest of two numbers.

---
+++ GreatestOfTwo.c
@@ -0,0 +1,15 @@
+// Write a program to find the greatest among 2 numbers.
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    printf("")
+    scanf("%d%d",&a,&b);
+    if(a>b){
+        printf("a is greater");
+    }else{
+        printf("b is greater");
+    }
+    return 0;
+}
